b7b65e502a7d60764807b3ab5ade8f0f962c3b90
g-yonchev <georgi.d.yonchev@gmail.com>
2015-10-07 04:10:02 +0300

    Created sample windows forms.

---
+++ MediaMonitoringSystem/MediaMonitoringSystem.WindowsFormsClient/Form1.cs
@@ -0,0 +1,31 @@
+﻿namespace MediaMonitoringSystem.WindowsFormsClient
+{
+    using System;
+    using System.IO.Compression;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                string zipPath = FD.FileName;
+                string extractPath = "../../../ImportedFiles";
+
+                ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+                //FileInfo File = new FileInfo(FD.FileName);
+                ////OR
+                //StreamReader reader = new StreamReader(fileToOpen);
+                ////etc...
+            }
+        }
+    }
+}
