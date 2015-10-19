2b294d80ac409b97a9db944071cf6b73986bca2c
g-yonchev <georgi.d.yonchev@gmail.com>
2015-10-19 13:42:14 +0300

    PDF.cs

---
+++ MediaMonitoringSystem/MediaMonitoringSystem.ConsoleClient/PDF.cs
@@ -0,0 +1,89 @@
+﻿namespace MediaMonitoringSystem.ConsoleClient
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using iTextSharp.text;
+    using iTextSharp.text.pdf;
+    using MediaMonitoringSystem.Data.MSSQL;
+
+    public class Pdf
+    {
+        public void GetPdf()
+        {
+            var db = new MediaMonitoringSystemData();
+
+            var groupsOfThemes = db.Themes.All()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Client = t.Client.Name,
+                    CountMedias = t.Package.CountMedias,
+                    StartDate = t.StartDate,
+                    EndDate = t.EndDate,
+                    Price = t.Package.PricePerMonth
+                })
+                .OrderBy(n => n.Name)
+                .GroupBy(gr => gr.StartDate)
+                .ToList();
+
+
+
+            var doc = new Document();
+            var stream = new FileStream("Test.pdf", FileMode.Create);
+
+            using (PdfWriter.GetInstance(doc, stream))
+            {
+                doc.Open();
+
+                var table = new PdfPTable(1);
+                table.AddCell("Themes");
+                doc.Add(Chunk.NEWLINE);
+
+
+                foreach (var gr in groupsOfThemes)
+                {
+                    table.AddCell(gr.Key.ToString());
+                    doc.Add(Chunk.NEWLINE);
+
+                    Console.WriteLine(gr.Key);
+                    var innerTable = new PdfPTable(5);
+
+                    innerTable.AddCell("Theme");
+                    innerTable.AddCell("Client Name");
+                    innerTable.AddCell("Count Of Medias");
+                    innerTable.AddCell("Price");
+                    innerTable.AddCell("Until");
+
+
+                    foreach (var item in gr)
+                    {
+                        innerTable.AddCell(item.Name.ToString());
+                        innerTable.AddCell(item.Client.ToString());
+                        innerTable.AddCell(item.CountMedias.ToString());
+                        innerTable.AddCell(item.Price.ToString());
+                        innerTable.AddCell(item.EndDate.ToString());
+
+                        doc.Add(Chunk.NEWLINE);
+                        Console.WriteLine(item.Client);
+                    }
+                    table.AddCell(innerTable);
+
+                }
+
+
+
+                doc.Add(table);
+
+                doc.Close();
+            }
+
+
+
+
+        }
+    }
+}
--- MediaMonitoringSystem/MediaMonitoringSystem.ConsoleClient/Startup.cs
+++ MediaMonitoringSystem/MediaMonitoringSystem.ConsoleClient/Startup.cs
@@ -2,38 +2,49 @@
 {
     using System;
     using System.Linq;
+    using iTextSharp.text.pdf;
+    using iTextSharp.text;
+    using iTextSharp.text.pdf.collection;
     using MediaMonitoringSystem.Data.Archievers;
+    using MediaMonitoringSystem.Data.Exporters;
     using MediaMonitoringSystem.Data.MSSQL;
+    using System.IO;
+    using MediaMonitoringSystem.Models.MSSQL;
 
     public class Startup
     {
         public static void Main()
         {
-            var mongo = new MongoModel();
-            var distributors = mongo.GenerateDistributors(3);
-            mongo.InsertToMongo(distributors);
+            //var mongo = new MongoModel();
+            //var distributors = mongo.GenerateDistributors(3);
+            //mongo.InsertToMongo(distributors);
 
-            var distributorsToSql = mongo.GetFromMongo();
-            Console.WriteLine(distributorsToSql);
+            //var distributorsToSql = mongo.GetFromMongo();
+            //Console.WriteLine(distributorsToSql);
 
-            var db = new MediaMonitoringSystemData();
+            //var db = new MediaMonitoringSystemData();
 
-            foreach (var d in distributorsToSql)
-            {
-                //throw new Exeption dont know why????
-                //db.MediaDistributors.Add(d);
-            }
+            //foreach (var d in distributorsToSql)
+            //{
+            //    //throw new Exeption dont know why????
+            //    //db.MediaDistributors.Add(d);
+            //}
 
             //Try this to create the files in Debug folder
 
             //DemoExporters.RunMe();
 
-            //Test Archiver UNcommend to test it
+            ////Test Archiver UNcommend to test it
 
             //IArchiever zipArchiver = new ZipArchiever();
             //string zipPath = "../../MOCK_DATA.zip";
             //string extractedPath = "../../Export/";
             //zipArchiver.UnArchieve(zipPath, extractedPath);
+
+
+            var pdf = new Pdf();
+            pdf.GetPdf();
+            
         }
     }
 }
